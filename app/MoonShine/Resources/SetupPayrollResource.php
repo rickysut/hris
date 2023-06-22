0308fa5d6398e732b4b18e7aac9208b7da6f18c9
rickysut <ricky.sutanto@gmail.com>
2023-06-22 16:52:19 +0700

    setup payroll

--- app/MoonShine/Resources/SetupPayrollResource.php
+++ app/MoonShine/Resources/SetupPayrollResource.php
@@ -8,6 +8,8 @@ use App\Models\SetupPayroll;
 use MoonShine\Resources\Resource;
 use MoonShine\Fields\ID;
 use MoonShine\Actions\FiltersAction;
+use MoonShine\Fields\Number;
+use MoonShine\Fields\Text;
 
 class SetupPayrollResource extends Resource
 {
@@ -50,7 +52,17 @@ class SetupPayrollResource extends Resource
 	public function fields(): array
 	{
 		return [
-		    ID::make()->sortable(),
+		    ID::make()->hideOnIndex()->sortable(),
+            Text::make('Name', 'name',  fn($item) => $item->name)->sortable(),
+            Number::make('Tunj. Masa Kerja', 't_masakerja',fn($item) => $item->t_masakerja),
+            Number::make('Tunj. Uang Makan', 't_uangmakan',fn($item) => $item->t_uangmakan),
+            Number::make('Tunj. Jabatan', 'tjtt',fn($item) => $item->tjtt),
+            Number::make('Tunj. Bensin', 't_bensin',fn($item) => $item->t_bensin),
+            Number::make('Tunj. Team', 't_team',fn($item) => $item->t_team),
+            Number::make('Pot. BPJS Kesehatan', 'bpjs_kes',fn($item) => $item->bpjs_kes),
+            Number::make('Pot. BPJS Tenaga kerja', 'bpjs_naker',fn($item) => $item->bpjs_naker),
+            Number::make('Pot. Lain-Lain', 'pot_lain',fn($item) => $item->pot_lain),
+
         ];
 	}
 
